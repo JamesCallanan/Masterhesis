f9afa096b2b6c08fbd8d3c27943ec416b9b7ab29
jamesc <jamesc@foodmarble.com>
2022-02-24 21:14:34 +0000

    Reintroduced up to train_loader return

--- data_loader.py
+++ data_loader.py
@@ -62,10 +62,7 @@ def organise_data_directories_and_return_datasets(  disease_classes = [ 'HCM', '
     # # Fn only moves images corresponding to disease classes in disease_classes array into the data/train directory
     # # Fn also returns a dictionary that contains a list of paths to the images that have been moved to the data/train directory and their corresponding ground truth (gt) segmentation maps in the unzipped_training_data_path 
     seg_masks_and_image_paths = move_some_training_files_to_data_train_directory(disease_classes, unzipped_training_data_path = unzipped_training_data_path, performROI = performROI)
-    return seg_masks_and_image_paths
     # ###########################################################################################################################
-
-    # #want to change previous code so files are only copied if in disease_classes
 
     # # Call process_scan on each file in train_data path scan is resized across height, width, and depth and rescaled.
     # # Classification labels vary depending on whether this is a binary or multiclass classification problem
@@ -73,149 +70,149 @@ def organise_data_directories_and_return_datasets(  disease_classes = [ 'HCM', '
 
     # binary_classification_label = 0
 
-    # x_train = []
-    # y_train = []
-    # filenames_train = []
+    x_train = []
+    y_train = []
+    filenames_train = []
 
-    # x_val = []
-    # y_val = []
-    # filenames_val = []
+    x_val = []
+    y_val = []
+    filenames_val = []
 
-    # if 'NOR' in disease_classes:
-    #     print('NOR entered')
-    #     NOR_training_folder_path = '/content/data/train/NOR/'
-    #     NOR_scan_paths = [ NOR_training_folder_path + x for x in os.listdir(NOR_training_folder_path)]
-    #     NOR_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in NOR_scan_paths])
-    #     if len(disease_classes) == 2:
-    #         NOR_labels = np.array([ binary_classification_label for _ in range(len(NOR_scans))])
-    #         binary_classification_label = binary_classification_label + 1   #increment for next use
-    #     elif len(disease_classes) > 2:
-    #         NOR_labels = np.array([ [1.0, 0.0, 0.0, 0.0, 0.0] for _ in range(len(NOR_scans))])
-    #     x_train = [*x_train, *NOR_scans[:16]]
-    #     y_train = [*y_train, *NOR_labels[:16]]
-    #     filenames_train = [*filenames_train, *NOR_scan_paths[:16]]
+    if 'NOR' in disease_classes:
+        print('NOR entered')
+        NOR_training_folder_path = '/content/data/train/NOR/'
+        NOR_scan_paths = [ NOR_training_folder_path + x for x in os.listdir(NOR_training_folder_path)]
+        NOR_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in NOR_scan_paths])
+        if len(disease_classes) == 2:
+            NOR_labels = np.array([ binary_classification_label for _ in range(len(NOR_scans))])
+            binary_classification_label = binary_classification_label + 1   #increment for next use
+        elif len(disease_classes) > 2:
+            NOR_labels = np.array([ [1.0, 0.0, 0.0, 0.0, 0.0] for _ in range(len(NOR_scans))])
+        x_train = [*x_train, *NOR_scans[:16]]
+        y_train = [*y_train, *NOR_labels[:16]]
+        filenames_train = [*filenames_train, *NOR_scan_paths[:16]]
 
-    #     x_val = [*x_val, *NOR_scans[16:]]
-    #     y_val = [*y_val, *NOR_labels[16:]]
-    #     filenames_val = [*filenames_val, *NOR_scan_paths[16:]]
+        x_val = [*x_val, *NOR_scans[16:]]
+        y_val = [*y_val, *NOR_labels[16:]]
+        filenames_val = [*filenames_val, *NOR_scan_paths[16:]]
 
-    # if 'DCM' in disease_classes:
-    #     print('DCM entered')
-    #     DCM_training_folder_path = '/content/data/train/DCM/'
-    #     DCM_scan_paths = [ DCM_training_folder_path + x for x in os.listdir(DCM_training_folder_path)]
-    #     DCM_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in DCM_scan_paths])
-    #     if len(disease_classes) == 2:
-    #         DCM_labels = np.array([ binary_classification_label for _ in range(len(DCM_scans))])
-    #         binary_classification_label = binary_classification_label + 1   #increment for next use
-    #     elif len(disease_classes) > 2:
-    #         DCM_labels = np.array([ [0.0, 1.0, 0.0, 0.0, 0.0] for _ in range(len(DCM_scans))])
-    #     x_train = [*x_train, *DCM_scans[:16]]
-    #     y_train = [*y_train, *DCM_labels[:16]]
-    #     filenames_train = [*filenames_train, *DCM_scan_paths[:16]]
+    if 'DCM' in disease_classes:
+        print('DCM entered')
+        DCM_training_folder_path = '/content/data/train/DCM/'
+        DCM_scan_paths = [ DCM_training_folder_path + x for x in os.listdir(DCM_training_folder_path)]
+        DCM_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in DCM_scan_paths])
+        if len(disease_classes) == 2:
+            DCM_labels = np.array([ binary_classification_label for _ in range(len(DCM_scans))])
+            binary_classification_label = binary_classification_label + 1   #increment for next use
+        elif len(disease_classes) > 2:
+            DCM_labels = np.array([ [0.0, 1.0, 0.0, 0.0, 0.0] for _ in range(len(DCM_scans))])
+        x_train = [*x_train, *DCM_scans[:16]]
+        y_train = [*y_train, *DCM_labels[:16]]
+        filenames_train = [*filenames_train, *DCM_scan_paths[:16]]
 
-    #     x_val = [*x_val, *DCM_scans[16:]]
-    #     y_val = [*y_val, *DCM_labels[16:]]
-    #     filenames_val = [*filenames_val, *DCM_scan_paths[16:]]
+        x_val = [*x_val, *DCM_scans[16:]]
+        y_val = [*y_val, *DCM_labels[16:]]
+        filenames_val = [*filenames_val, *DCM_scan_paths[16:]]
 
-    # if 'HCM' in disease_classes:
-    #     print('HCM entered')
-    #     HCM_training_folder_path = '/content/data/train/HCM/'
-    #     HCM_scan_paths = [ HCM_training_folder_path + x for x in os.listdir(HCM_training_folder_path)]
-    #     HCM_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in HCM_scan_paths])
-    #     if len(disease_classes) == 2:
-    #         HCM_labels = np.array([ binary_classification_label for _ in range(len(HCM_scans))])
-    #         binary_classification_label = binary_classification_label + 1   #increment for next use
-    #     elif len(disease_classes) > 2:
-    #         HCM_labels = np.array([ [0.0, 0.0, 1.0, 0.0, 0.0] for _ in range(len(HCM_scans))])
-    #     x_train = [*x_train, *HCM_scans[:16]]
-    #     y_train = [*y_train, *HCM_labels[:16]]
-    #     filenames_train = [*filenames_train, *HCM_scan_paths[:16]]
+    if 'HCM' in disease_classes:
+        print('HCM entered')
+        HCM_training_folder_path = '/content/data/train/HCM/'
+        HCM_scan_paths = [ HCM_training_folder_path + x for x in os.listdir(HCM_training_folder_path)]
+        HCM_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in HCM_scan_paths])
+        if len(disease_classes) == 2:
+            HCM_labels = np.array([ binary_classification_label for _ in range(len(HCM_scans))])
+            binary_classification_label = binary_classification_label + 1   #increment for next use
+        elif len(disease_classes) > 2:
+            HCM_labels = np.array([ [0.0, 0.0, 1.0, 0.0, 0.0] for _ in range(len(HCM_scans))])
+        x_train = [*x_train, *HCM_scans[:16]]
+        y_train = [*y_train, *HCM_labels[:16]]
+        filenames_train = [*filenames_train, *HCM_scan_paths[:16]]
 
-    #     x_val = [*x_val, *HCM_scans[16:]]
-    #     y_val = [*y_val, *HCM_labels[16:]]
-    #     filenames_val = [*filenames_val, *HCM_scan_paths[16:]]
+        x_val = [*x_val, *HCM_scans[16:]]
+        y_val = [*y_val, *HCM_labels[16:]]
+        filenames_val = [*filenames_val, *HCM_scan_paths[16:]]
 
-    # if 'MINF' in disease_classes:
-    #     print('MINF entered')
-    #     MINF_training_folder_path = '/content/data/train/MINF/'
-    #     MINF_scan_paths = [ MINF_training_folder_path + x for x in os.listdir(MINF_training_folder_path)]
-    #     MINF_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in MINF_scan_paths])
-    #     if len(disease_classes) == 2:
-    #         MINF_labels = np.array([ binary_classification_label for _ in range(len(MINF_scans))])
-    #         binary_classification_label = binary_classification_label + 1   #increment for next use
-    #     elif len(disease_classes) > 2:
-    #         MINF_labels = np.array([ [0.0, 0.0, 0.0, 1.0, 0.0] for _ in range(len(MINF_scans))])
-    #     x_train = [*x_train, *MINF_scans[:16]]
-    #     y_train = [*y_train, *MINF_labels[:16]]
-    #     filenames_train = [*filenames_train, *MINF_scan_paths[:16]]
+    if 'MINF' in disease_classes:
+        print('MINF entered')
+        MINF_training_folder_path = '/content/data/train/MINF/'
+        MINF_scan_paths = [ MINF_training_folder_path + x for x in os.listdir(MINF_training_folder_path)]
+        MINF_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in MINF_scan_paths])
+        if len(disease_classes) == 2:
+            MINF_labels = np.array([ binary_classification_label for _ in range(len(MINF_scans))])
+            binary_classification_label = binary_classification_label + 1   #increment for next use
+        elif len(disease_classes) > 2:
+            MINF_labels = np.array([ [0.0, 0.0, 0.0, 1.0, 0.0] for _ in range(len(MINF_scans))])
+        x_train = [*x_train, *MINF_scans[:16]]
+        y_train = [*y_train, *MINF_labels[:16]]
+        filenames_train = [*filenames_train, *MINF_scan_paths[:16]]
 
-    #     x_val = [*x_val, *MINF_scans[16:]]
-    #     y_val = [*y_val, *MINF_labels[16:]]
-    #     filenames_val = [*filenames_val, *MINF_scan_paths[16:]]
+        x_val = [*x_val, *MINF_scans[16:]]
+        y_val = [*y_val, *MINF_labels[16:]]
+        filenames_val = [*filenames_val, *MINF_scan_paths[16:]]
 
-    # if 'RV' in disease_classes:
-    #     print('RV entered')
-    #     RV_training_folder_path = '/content/data/train/RV/'
-    #     RV_scan_paths = [ RV_training_folder_path + x for x in os.listdir(RV_training_folder_path)]
-    #     RV_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in RV_scan_paths])
-    #     if len(disease_classes) == 2:
-    #         RV_labels = np.array([ binary_classification_label for _ in range(len(RV_scans))])
-    #         binary_classification_label = binary_classification_label + 1   #increment for next use
-    #     elif len(disease_classes) > 2:
-    #         RV_labels = np.array([ [0.0, 0.0, 0.0, 0.0, 1.0] for _ in range(len(RV_scans))])
-    #     x_train = [*x_train, *RV_scans[:16]]
-    #     y_train = [*y_train, *RV_labels[:16]]
-    #     filenames_train = [*filenames_train, *RV_scan_paths[:16]]
+    if 'RV' in disease_classes:
+        print('RV entered')
+        RV_training_folder_path = '/content/data/train/RV/'
+        RV_scan_paths = [ RV_training_folder_path + x for x in os.listdir(RV_training_folder_path)]
+        RV_scans = np.array([process_scan( path , desired_depth = 10, desired_height = 90, desired_width = 90) for path in RV_scan_paths])
+        if len(disease_classes) == 2:
+            RV_labels = np.array([ binary_classification_label for _ in range(len(RV_scans))])
+            binary_classification_label = binary_classification_label + 1   #increment for next use
+        elif len(disease_classes) > 2:
+            RV_labels = np.array([ [0.0, 0.0, 0.0, 0.0, 1.0] for _ in range(len(RV_scans))])
+        x_train = [*x_train, *RV_scans[:16]]
+        y_train = [*y_train, *RV_labels[:16]]
+        filenames_train = [*filenames_train, *RV_scan_paths[:16]]
 
-    #     x_val = [*x_val, *RV_scans[16:]]
-    #     y_val = [*y_val, *RV_labels[16:]]
-    #     filenames_val = [*filenames_val, *RV_scan_paths[16:]]
+        x_val = [*x_val, *RV_scans[16:]]
+        y_val = [*y_val, *RV_labels[16:]]
+        filenames_val = [*filenames_val, *RV_scan_paths[16:]]
 
-    # print('disease_classes: ', disease_classes)
-    # print(len(x_train))
-    # print(len(x_val))
-    # print(len(y_train))
-    # print(len(y_val))
-    # print(filenames_train)
-    # print(filenames_val)
-    # #convert to numpy arrays
-    # x_train = np.array(x_train)
-    # y_train = np.array(y_train)
-    # filenames_train = np.array(filenames_train)
+    print('disease_classes: ', disease_classes)
+    print(len(x_train))
+    print(len(x_val))
+    print(len(y_train))
+    print(len(y_val))
+    print(filenames_train)
+    print(filenames_val)
+    #convert to numpy arrays
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    filenames_train = np.array(filenames_train)
 
-    # x_val = np.array(x_val)
-    # y_val = np.array(y_val)
-    # filenames_val = np.array(filenames_val)
+    x_val = np.array(x_val)
+    y_val = np.array(y_val)
+    filenames_val = np.array(filenames_val)
 
-    # # def train_preprocessing(volume, label, paths):
-    # #     """Process training data by rotating and adding a channel."""
-    # #     #volume = augment(volume)
-    # #     volume = tf.expand_dims(volume, axis=3)
-    # #     return volume, label, paths
+    # def train_preprocessing(volume, label, paths):
+    #     """Process training data by rotating and adding a channel."""
+    #     #volume = augment(volume)
+    #     volume = tf.expand_dims(volume, axis=3)
+    #     return volume, label, paths
 
-    # # def validation_preprocessing(volume, label, paths):
-    # #     """Process validation data by only adding a channel."""
-    # #     volume = tf.expand_dims(volume, axis=3)
-    # #     return volume, label, paths
+    # def validation_preprocessing(volume, label, paths):
+    #     """Process validation data by only adding a channel."""
+    #     volume = tf.expand_dims(volume, axis=3)
+    #     return volume, label, paths
 
-    # # Define data loaders.
-    # train_loader = tf.data.Dataset.from_tensor_slices((x_train, y_train, filenames_train))
-    # validation_loader = tf.data.Dataset.from_tensor_slices((x_val, y_val, filenames_val))
-    # return train_loader, validation_loader, x_train, y_train
-    # # # Augment the on the fly during training.
-    # # train_dataset = (
-    # #     train_loader.shuffle(len(x_train))
-    # #     .map(train_preprocessing)
-    # #     .batch(train_batch_size)
-    # #     .prefetch(2)
-    # # )
+    # Define data loaders.
+    train_loader = tf.data.Dataset.from_tensor_slices((x_train, y_train, filenames_train))
+    validation_loader = tf.data.Dataset.from_tensor_slices((x_val, y_val, filenames_val))
+    return train_loader, validation_loader, x_train, y_train
+    # # Augment the on the fly during training.
+    # train_dataset = (
+    #     train_loader.shuffle(len(x_train))
+    #     .map(train_preprocessing)
+    #     .batch(train_batch_size)
+    #     .prefetch(2)
+    # )
 
-    # # # # Only rescale.
-    # # validation_dataset = (
-    # #     validation_loader.shuffle(len(x_val))
-    # #     .map(validation_preprocessing)
-    # #     .batch(validation_batch_size)
-    # #     .prefetch(2)
-    # # )
+    # # # Only rescale.
+    # validation_dataset = (
+    #     validation_loader.shuffle(len(x_val))
+    #     .map(validation_preprocessing)
+    #     .batch(validation_batch_size)
+    #     .prefetch(2)
+    # )
 
-    # # return train_dataset, validation_dataset
+    # return train_dataset, validation_dataset
