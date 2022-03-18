4e7965a52ff032a8be6f7c20531187b258e30711
jamesc <jamesc@foodmarble.com>
2022-03-18 11:27:32 +0000

    Keeping some constants that were in colab notebook in config file

--- config.py
+++ config.py
@@ -1,5 +1,18 @@
+from enum import Enum
+
 training_directory = '/content/data/train/'
 validation_directory = '/content/data/validation/'
 datasets_wanted = ['train/','validation/']
 base_training_data_path = '/content/training'
 number_of_patients_per_class = 20
+
+KT_dir = '/content/gdrive/MyDrive/ME Project/Results/Keras_Tuner/'
+tensorboard_folder_path = '/content/gdrive/MyDrive/ME Project/Results/TensorBoard/'
+model_base_path = '/content/gdrive/MyDrive/ME Project/Results/Models/'
+model_info_json = '/content/gdrive/MyDrive/ME Project/Results/model_info.json'
+
+class Model_Modes(Enum):
+  STANDARD = 'STANDARD'
+  ZEROS_OUTSIDE_HEART_TRAIN = 'ZEROS_OUTSIDE_HEART_TRAIN'
+  ZEROS_OUTSIDE_HEART_TRAIN_VAL = 'ZEROS_OUTSIDE_HEART_TRAIN_VAL'
+  GRAD_CAM_LOSS_FN = 'GRAD_CAM_LOSS_FN'
