90c54306bf6d377ba8dc701b8ce24c2292d8e928
jamesc <jamesc@foodmarble.com>
2022-03-19 19:02:12 +0000

    Corrected two of the db queries

--- db_query_fns.py
+++ db_query_fns.py
@@ -22,27 +22,28 @@ def insert_tuner_search(search, database_connection_details):
                           tensorboard_folder_path,
                           keras_tuner_folder_path 
                         ) 
-                        VALUES
-                        (
-                          '{search['search_id']}',
-                          {search['search_type']},
-                          {search['num_models']},
-                          {search['num_epochs']},
-                          '{search['model_template_builder_name']}',
-                          '{search['hyperparam_ranges']}',
-                          {search['disease_classes']},
-                          {search['model_mode']},
-                          {search['perform_ROI']},
-                          {search['depth']},
-                          {search['width']},
-                          {search['height']},
-                          '{search['git_commit_id']}',
-                          '{search['git_branch']}',
-                          '{search['tensorboard_folder_path']}',
-                          '{search['keras_tuner_folder_path']}'
-                        )
+                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
                         ON CONFLICT DO NOTHING
-                        """)
+                        """, 
+                        ( 
+                          search['search_id'],
+                          search['search_type'],
+                          search['num_models'],
+                          search['num_epochs'],
+                          search['model_template_builder_name'],
+                          search['hyperparam_ranges'],
+                          search['disease_classes'],
+                          search['model_mode'],
+                          search['perform_ROI'],
+                          search['depth'],
+                          search['width'],
+                          search['height'],
+                          search['git_commit_id'],
+                          search['git_branch'],
+                          search['tensorboard_folder_path'],
+                          search['keras_tuner_folder_path'] 
+                        )                       
+                       )
     conn.close()
 
 
@@ -51,23 +52,19 @@ def insert_trial(trial, database_connection_details):
     cursor = conn.cursor()
     with conn:
         cursor.execute(f"""INSERT INTO trials 
-                        ( trial_id,
-                          search_id,
-                          model_path,
-                          average_fraction_of_heart_in_mri_batch,
-                          average_fraction_of_pos_gradients_in_heart_in_batch_of_mris,
-                          average_fraction_of_neg_gradients_in_heart_in_batch_of_mris
+                          ( trial_id,
+                            search_id,
+                            model_path
+                          )
+                          VALUES (%s,%s,%s)
+                          ON CONFLICT DO NOTHING
+                        """,
+                        (
+                          trial['trial_id'],
+                          trial['search_id'],
+                          trial['model_path']
                         )
-                        VALUES 
-                        ( '{trial['trial_id']}',
-                          {trial['search_id']}, 
-                          '{trial['model_path']}', 
-                          {trial['average_fraction_of_heart_in_mri_batch']}, 
-                          {trial['average_fraction_of_pos_gradients_in_heart_in_batch_of_mris']}, 
-                          {trial['average_fraction_of_neg_gradients_in_heart_in_batch_of_mris']}
-                        )
-                        ON CONFLICT DO NOTHING
-                      """)
+                      )
     conn.close()
 
 
