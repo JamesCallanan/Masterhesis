1e93d9230aabeeb2ad7091d4bb07fbb1cca2f921
jamesc <jamesc@foodmarble.com>
2022-03-19 21:17:47 +0000

    Renamed fn so its unique - silly mistake

--- config.py
+++ config.py
@@ -40,36 +40,6 @@ class Order_By(Enum):
   ASC = 'asc'
   DESC = 'desc'
 
-class Tuner_Search_table_column_indexes(Enum):
-  search_id = 0
-  search_type = 1 
-  num_models = 2
-  num_epochs = 3
-  model_template_builder_name = 4
-  hyperparam_ranges = 5
-  disease_classes = 6
-  model_mode = 7
-  perform_ROI = 8
-  depth = 9
-  width = 10
-  height = 11
-  git_commit_id = 12
-  git_branch = 13
-  tensorboard_folder_path = 14
-  keras_tuner_folder_path = 15
-
-class Trials_table_column_indexes(Enum):
-  trial_id = 0
-  search_id = 1
-  model_path = 2
-  val_loss = 3
-  val_acc = 4
-  train_loss = 5
-  train_acc = 6
-  last_conv_layer_name = 7
-  average_fraction_of_heart_in_mri_batch = 8
-  average_fraction_of_pos_gradients_in_heart_in_batch_of_mris = 9
-  average_fraction_of_neg_gradients_in_heart_in_batch_of_mris = 10
 
 
 def should_we_hide_pixels_outside_heart(model_mode_value):
@@ -91,3 +61,35 @@ def should_we_hide_pixels_outside_heart(model_mode_value):
       hide_pixels_outside_heart_val = False
 
   return hide_pixels_outside_heart_train, hide_pixels_outside_heart_val
+
+
+# class Tuner_Search_table_column_indexes(Enum):
+#   search_id = 0
+#   search_type = 1 
+#   num_models = 2
+#   num_epochs = 3
+#   model_template_builder_name = 4
+#   hyperparam_ranges = 5
+#   disease_classes = 6
+#   model_mode = 7
+#   perform_ROI = 8
+#   depth = 9
+#   width = 10
+#   height = 11
+#   git_commit_id = 12
+#   git_branch = 13
+#   tensorboard_folder_path = 14
+#   keras_tuner_folder_path = 15
+
+# class Trials_table_column_indexes(Enum):
+#   trial_id = 0
+#   search_id = 1
+#   model_path = 2
+#   val_loss = 3
+#   val_acc = 4
+#   train_loss = 5
+#   train_acc = 6
+#   last_conv_layer_name = 7
+#   average_fraction_of_heart_in_mri_batch = 8
+#   average_fraction_of_pos_gradients_in_heart_in_batch_of_mris = 9
+#   average_fraction_of_neg_gradients_in_heart_in_batch_of_mris = 10
