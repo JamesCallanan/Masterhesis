bc01e74920a43181e6a27a028b73c24b3f1b105e
jamesc <jamesc@foodmarble.com>
2022-03-22 13:58:25 +0000

    Added in flipping of images

--- augmentations.py
+++ augmentations.py
@@ -14,7 +14,7 @@ def rotate_xy_plane(volume):
 
 # Taken from here https://github.com/ashawkey/volumentations/blob/master/volumentations/augmentations/functionals.py
 def gamma_transform(volume, gamma, eps=1e-7):
-  if np.random.uniform(0,1) > 0:
+  if np.random.uniform(0,1) > 0.5:
     mn = volume.min()
     rng = volume.max() - mn
     volume = (volume - mn)/(rng + eps)
@@ -35,8 +35,11 @@ def augment(volume):
     """Rotate the volume by a few degrees"""
     def augment(volume):
       volume = rotate_xy_plane(volume)
-      volume = gamma_transform(volume, gamma=0.9, eps=1e-7)
+      volume = gamma_transform(volume, gamma=0.85, eps=1e-7)
       volume = gaussian_noise(volume)
+      if np.random.uniform(0,1) > 0.5:
+        volume = np.fliplr(volume)
+      
       return np.ndarray.astype(volume, np.float32) # Had to make this change https://stackoverflow.com/questions/54278894/0-th-value-returned-by-pyfunc-0-is-double-but-expects-float-though-i-think-it
 
     augmented_volume = tf.numpy_function(augment, [volume], tf.float32)
