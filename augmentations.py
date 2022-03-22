6a18a2f48259875b4562e93f6c00e1ebee738f55
jamesc <jamesc@foodmarble.com>
2022-03-22 15:11:09 +0000

    Fixed error portentially

--- augmentations.py
+++ augmentations.py
@@ -86,7 +86,7 @@ def augment(volume):
       # volume = rotate_xy_plane(volume)
       # volume = gamma_transform(volume, gamma_range=[0.85,1.15], eps=1e-7)
       # volume = gaussian_noise(volume)
-      volume, seg = motion_augmentation(volume, seg=None, p_augm=0.5, mu=0, sigma_multiplier = 0.22)
+      volume, seg = motion_augmentation(volume, seg=None, p_augm=1, mu=0, sigma_multiplier = 0.22)
 
       if np.random.uniform(0,1) > 0.5:
         volume = np.fliplr(volume)
