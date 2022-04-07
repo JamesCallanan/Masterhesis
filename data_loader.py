70c6f5222fc39d38522047951c360edd2b5bec60
jamesc <jamesc@foodmarble.com>
2022-04-07 11:22:56 +0100

    removed redundant code from this

--- data_loader.py
+++ data_loader.py
@@ -34,12 +34,6 @@ def organise_data_directories_and_return_datasets(  disease_classes = [ 'HCM', '
         desired_depth = desired_dimensions['full_image']['desired_depth']   
         desired_width = desired_dimensions['full_image']['desired_width']   # mean value for non ROI images is ~220
         desired_height = desired_dimensions['full_image']['desired_height']  # mean value for non ROI images is ~247
-    
-    if 'ABNOR' in disease_classes and 'NOR' in disease_classes:
-        NOR_ABNOR_classification = True
-    else:
-        NOR_ABNOR_classification = False
-    
 
     zipped_training_data_path = base_training_data_path + '.zip'
     unzipped_training_data_path = base_training_data_path + '/'
